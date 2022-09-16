0b2b5bb825fc4d472fd0f6a721286c0ef9cc1043
villetoimela <ville.toimela@hiisi.digital>
2022-09-16 09:47:52 +0300

    QuizQuestions component added

---
+++ src/assets/QuizQuestions.js
@@ -0,0 +1,27 @@
+export const quizQuestions = (
+    userSelectedOperatorsParams,
+    userSelectedQuestionCount,
+    userSelectedRandomRange,
+) => {
+    const questions = []
+
+    const questionsCount = userSelectedQuestionCount;
+    const operators = userSelectedOperatorsParams;
+
+    for (let i = 0; i < questionsCount; i++) {
+
+        let randomNumberOne = Math.ceil(Math.random() * userSelectedRandomRange);
+        let randomNumberTwo = Math.ceil(Math.random() * userSelectedRandomRange);
+        let operator = operators[Math.floor(Math.random() * operators.lenght)];
+
+        let mathQuiz = randomNumberOne + operator + randomNumberTwo;
+
+        questions.push({
+            question: mathQuiz,
+            answer: operator === "/" ? eval(mathQuiz).toFixed() : eval(mathQuiz),
+        });
+    }
+
+    return questions;
+};
+
